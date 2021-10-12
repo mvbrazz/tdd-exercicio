af4f5d1e7f24178e991a7f2dd450de19e4778a9f
Marcus Vinicus <mvbrazz@hotmail.com>
2021-10-12 18:39:46 -0300

    Commit test DBA

--- test/Atividade3.test.js
+++ test/Atividade3.test.js
@@ -60,4 +60,62 @@ describe('Atv3',()=>{
         expect(aux.salario_liquido).toBe(1800);
     });
 
+    test('DBA com salário maior ou igual a 2000',()=>{
+        let funcionario = {};
+        funcionario.nome = "Milena";
+        funcionario.e_mail = "funcionario@gmail.com";
+        funcionario.salario_base=2000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
+        funcionario.cargo="DBA";
+
+        var aux = verificaDesenvolvedor(funcionario);
+        
+        expect(aux.cargo).toBe("DBA");
+        expect(aux.tipo_salario).toBe(0);
+    });
+
+    test('Desenvolvedor com salário menor que 2000',()=>{
+        let funcionario = {};
+        funcionario.nome = "Milena";
+        funcionario.e_mail = "funcionario@gmail.com";
+        funcionario.salario_base=1000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
+        funcionario.cargo="DBA";
+
+        var aux = verificaDesenvolvedor(funcionario);
+        
+        expect(aux.cargo).toBe("DBA");
+        expect(aux.tipo_salario).toBe(1);
+    });
+
+    test('Desenvolvedor com salário liquido correto maior  ou igual a 3000',()=>{
+        let funcionario = {};
+        funcionario.nome = "Milena";
+        funcionario.e_mail = "funcionario@gmail.com";
+        funcionario.salario_base=2000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
+        funcionario.cargo="DBA";
+
+        var aux = verificaDesenvolvedor(funcionario);
+        
+        expect(aux.salario_liquido).toBe(1500);
+    });
+
+    test('Desenvolvedor com salário liquido correto menor que 3000',()=>{
+        let funcionario = {};
+        funcionario.nome = "Milena";
+        funcionario.e_mail = "funcionario@gmail.com";
+        funcionario.salario_base=1000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
+        funcionario.cargo="DBA";
+
+        var aux = verificaDesenvolvedor(funcionario);
+        
+        expect(aux.salario_liquido).toBe(850);
+    });
+
 })
