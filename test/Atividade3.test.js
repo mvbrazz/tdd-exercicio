61b394a6cfe52908fd8ea6d97b4fb2bde5e21a81
Marcus Vinicus <mvbrazz@hotmail.com>
2021-10-12 18:18:09 -0300

    Commit test passes Desenvolvedor

--- test/Atividade3.test.js
+++ test/Atividade3.test.js
@@ -1,17 +1,20 @@
 const { test } = require("@jest/globals");
+const verificaDesenvolvedor = require('../lib/Atividade3');
 
 describe('Atv3',()=>{
-    test('Desenvolvedor com salário maior que 3000',()=>{
+    test('Desenvolvedor com salário maior ou igual a 3000',()=>{
         let funcionario = {};
         funcionario.nome = "Milena";
         funcionario.e_mail = "funcionario@gmail.com";
         funcionario.salario_base=3000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
         funcionario.cargo="DESENVOLVEDOR";
 
         var aux = verificaDesenvolvedor(funcionario);
         
         expect(aux.cargo).toBe("DESENVOLVEDOR");
-        expect(aux.TipoSalario).toBe(0);
+        expect(aux.tipo_salario).toBe(0);
     });
 
     test('Desenvolvedor com salário menor que 3000',()=>{
@@ -19,24 +22,28 @@ describe('Atv3',()=>{
         funcionario.nome = "Milena";
         funcionario.e_mail = "funcionario@gmail.com";
         funcionario.salario_base=2000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
         funcionario.cargo="DESENVOLVEDOR";
 
         var aux = verificaDesenvolvedor(funcionario);
         
         expect(aux.cargo).toBe("DESENVOLVEDOR");
-        expect(aux.TipoSalario).toBe(1);
+        expect(aux.tipo_salario).toBe(1);
     });
 
-    test('Desenvolvedor com salário liquido correto maior que 3000',()=>{
+    test('Desenvolvedor com salário liquido correto maior  ou igual a 3000',()=>{
         let funcionario = {};
         funcionario.nome = "Milena";
         funcionario.e_mail = "funcionario@gmail.com";
         funcionario.salario_base=3000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
         funcionario.cargo="DESENVOLVEDOR";
 
         var aux = verificaDesenvolvedor(funcionario);
         
-        expect(aux.SalarioLiquido).toBe(2400);
+        expect(aux.salario_liquido).toBe(2400);
     });
 
     test('Desenvolvedor com salário liquido correto menor que 3000',()=>{
@@ -44,11 +51,13 @@ describe('Atv3',()=>{
         funcionario.nome = "Milena";
         funcionario.e_mail = "funcionario@gmail.com";
         funcionario.salario_base=2000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
         funcionario.cargo="DESENVOLVEDOR";
 
         var aux = verificaDesenvolvedor(funcionario);
         
-        expect(aux.SalarioLiquido).toBe(1800);
+        expect(aux.salario_liquido).toBe(1800);
     });
 
 })
