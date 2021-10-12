1eec23dc018987ec62801ff0d5b90356c4521a4a
Marcus Vinicus <mvbrazz@hotmail.com>
2021-10-12 19:04:51 -0300

    Commit test GERENTE

--- test/Atividade3.test.js
+++ test/Atividade3.test.js
@@ -176,4 +176,62 @@ describe('Atv3',()=>{
         expect(aux.salario_liquido).toBe(850);
     });    
 
+    test('GERENTE com salário maior ou igual a 5000',()=>{
+        let funcionario = {};
+        funcionario.nome = "Milena";
+        funcionario.e_mail = "funcionario@gmail.com";
+        funcionario.salario_base=5000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
+        funcionario.cargo="GERENTE";
+
+        var aux = verificaFuncionario(funcionario);
+        
+        expect(aux.cargo).toBe("GERENTE");
+        expect(aux.tipo_salario).toBe(0);
+    });
+
+    test('GERENTE com salário menor que 5000',()=>{
+        let funcionario = {};
+        funcionario.nome = "Milena";
+        funcionario.e_mail = "funcionario@gmail.com";
+        funcionario.salario_base=4000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
+        funcionario.cargo="GERENTE";
+
+        var aux = verificaFuncionario(funcionario);
+        
+        expect(aux.cargo).toBe("GERENTE");
+        expect(aux.tipo_salario).toBe(1);
+    });
+
+    test('GERENTE com salário liquido correto maior  ou igual a 5000',()=>{
+        let funcionario = {};
+        funcionario.nome = "Milena";
+        funcionario.e_mail = "funcionario@gmail.com";
+        funcionario.salario_base=2000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
+        funcionario.cargo="GERENTE";
+
+        var aux = verificaFuncionario(funcionario);
+        
+        expect(aux.salario_liquido).toBe(1500);
+    });
+
+    test('GERENTE com salário liquido correto menor que 5000',()=>{
+        let funcionario = {};
+        funcionario.nome = "Milena";
+        funcionario.e_mail = "funcionario@gmail.com";
+        funcionario.salario_base=4000.00;
+        funcionario.salario_liquido=0;
+        funcionario.tipo_salario=0;
+        funcionario.cargo="GERENTE";
+
+        var aux = verificaFuncionario(funcionario);
+        
+        expect(aux.salario_liquido).toBe(850);
+    }); 
+
 })
